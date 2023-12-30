dd32d8585f36295c9627a02558f91821458a2fc4
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-30 12:19:24 +0530

    datatypes (integer) in python (P1)

---
+++ python004.py
@@ -0,0 +1,11 @@
+#datatypes and variables in python
+#datatype(number) and displaying its typetr
+num1 = 12
+num2 = 12.001
+num3 = 12j
+num4 = True
+
+print("Type of num1 = ", type(num1))
+print("Type of num2 = ", type(num2))
+print("Type of num3 = ", type(num3))
+print("Type of num4 = ", type(num4))
