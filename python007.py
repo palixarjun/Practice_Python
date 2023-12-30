74e8efd83333aabd746d1927e7cd59028af4d24c
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-30 12:21:46 +0530

    assigning multiple values at once

---
+++ python007.py
@@ -0,0 +1,23 @@
+#assigning multiple values at once
+#& assigning single value to multiple instances at once
+print("a, b, c, d = 123, 1234, 12345, 'String'")
+a, b, c, d = 123, 1234, 12345, 'String'
+print("a = ",a)
+print("b = ",b)
+print("c = ",c)
+print("d = ",d)
+print("\n")
+
+print("e = f = g = h = 123")
+e = f = g = h = 123
+print("e = ",a)
+print("f = ",f)
+print("g = ",g)
+print("h = ",h)
+print("\n")
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
