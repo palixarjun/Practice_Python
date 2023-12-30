ac58c0d7c67ebd23b2c786e6853857b769aed74f
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-30 12:21:01 +0530

    casting in python and variable naming methods

---
+++ python006.py
@@ -0,0 +1,21 @@
+#casting in python
+#to specify the input type or data type of a particular entity
+
+a = int(2)
+b = float(2)
+c = bool(1)
+d = str(3)
+
+list = [a, b, c, d]
+for i in list:
+    print(i , " = ", type(i))
+    
+#defining a variable
+#camelcase (each letter expect the first letter is capital)
+myVariable1 = 123123
+
+#pascal case(each letter is capitalised)
+MyVariable2 = 'String 1'
+
+#snake case (using the underscores)
+my_variable_3 = 1212123
