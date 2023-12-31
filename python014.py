b2a9f0d9a1d834b1f69475ad39e60b1b4ee24e5c
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-31 11:14:08 +0530

    operations on strings (P4)

---
+++ python014.py
@@ -0,0 +1,34 @@
+#other operations performed over strings are
+"""
+format()
+format_map()
+index()
+isalnum()
+isaplha()
+isascii()
+isdecimal() #returns False for float
+isdigit() #returns False for float
+split()
+splitlines()
+isidentifier()
+isprintable() #returns False when the string contains new line character, return character
+isspace()
+istitle()
+isupper()
+islower()
+join()
+!just() #justifies the string to left
+lower() #returns to the lowercase
+isnumeric()
+strip()
+swapcase()
+title()
+translate()
+upper()
+zfill()
+rsplit()
+rpartition()
+rstrip()
+rfind()
+rindex()
+"""
