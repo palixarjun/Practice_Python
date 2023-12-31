1cec1a8b4bee536eda97cf83937b9a94e75b29c3
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-31 11:46:10 +0530

    operators in python (P1)

---
+++ python016.py
@@ -0,0 +1,27 @@
+#basic arithematic operations in python
+#+ addition
+#- subtraction
+#/ division
+#* mutliplication
+#// floor division
+#** exponent
+#% modula
+
+a = 12
+b = 5
+c = 2
+print('a = 12')
+print('b = 5')
+print('c = 2')
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
+print("a//b =", a//b, '#returns the quotient')
+print("a**b =", a**b)
+
+
+#assignment operators
+#+= *= -= /= %= //== >>== <<==
+c *= b
+print("c*=b", (c))
