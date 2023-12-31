a69afe8912f222d9bcea78b6e59131f7d9d33c43
Arjun Pali <145116301+palixarjun@users.noreply.github.com>
2023-12-31 10:44:27 +0530

    check operations on Strings

---
+++ python010.py
@@ -0,0 +1,11 @@
+#to check in the strings 
+
+txt = "This is a statement. It contains some random words like Free, tree, bee, see, sea, etc."
+
+#check if free in txt String
+if ('free' or 'Free' in txt):
+    print("free or Free is present in the String.")
+    
+if ('Random' not in txt):
+    print("'Random' is not present in the txt.")
+    
