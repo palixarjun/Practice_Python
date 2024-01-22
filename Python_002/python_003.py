633ba6e605529060799c84c89cdc1cd871558057
palixarjun <palixarjun@gmail.com>
2024-01-22 09:37:11 +0530

    ???

---
+++ Python_002/python_003.py
@@ -0,0 +1,7 @@
+#convert the degrees to radian and vice versa
+
+x = float(input("enter the value in degrees :"))
+# 1pi = 180 degress
+# 1 rad = 180/3.14
+y = x*(3.14/180)
+print(("{} degrees = {} rad").format(x,y))
